521680eb7647d51d93969cd06707bbf532953384
Frobeniusnorm <david@frobeniusnorm.de>
2024-05-28 18:30:57 +0200

    sort of works, but it is weird

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -21,22 +21,7 @@
 
 using namespace sycl;
 const unsigned char COLOR_SET1[][3] = {
-	{255, 255, 255},
-	{240, 255, 230},
-	{230, 255, 220},
-	{200, 255, 230},
-	{150, 250, 200},
-	{100, 250, 150},
-	{80, 240, 150},
-	{60, 230, 180},
-	{50, 210, 190},
-	{50, 200, 200},
-	{50, 190, 210},
-	{50, 190, 230},
-	{50, 190, 240},
-	{50, 150, 240},
-	{50, 130, 230}
-};
+    {255, 255, 255}, {100, 250, 150}, {50, 200, 200}, {50, 130, 230}};
 static size_t max_iterations(double xlims) {
   // no mathematical proof, just approximation
   return 50 + (long)sycl::pow(sycl::log10(((4. / xlims))), 5);
@@ -62,17 +47,41 @@ void mandelbrot(queue &Q, double x_min, double x_max, double y_min,
       const size_t max_iter = max_iterations(sycl::fabs(x_max - x_min));
       // simulate complex conjecture
       size_t iter = 0;
-      for (; iter < max_iter && c_x * c_x + c_y * c_y <= 2 * 2; iter++) {
+      for (; iter < max_iter && c_x * c_x + c_y * c_y <= (1 << 16); iter++) {
         const double new_x = c_x * c_x - c_y * c_y + x0;
         const double new_y = 2 * c_x * c_y + y0;
         c_x = new_x;
         c_y = new_y;
       }
-      // map to color map, higher iterations -> higher index
-      int color_idx = (int)((iter / (double)max_iter) *
-                            (sizeof(COLOR_SET1) / (sizeof(char) * 3)));
-      for (int j = 0; j < 3; j++) {
-        img[i * 3 + j] = COLOR_SET1[color_idx][j];
+      // apply smoothing
+      if (iter < max_iter) {
+        const double log_zn = sycl::log(c_x * c_x + c_y * c_y) / 2;
+        const double nu = sycl::log2(log_zn / sycl::log(2.));
+        const double iters = iter + 1 - nu;
+        // map to color map, higher iterations -> higher index
+        const int NUM_COLORS = (sizeof(COLOR_SET1) / (sizeof(char) * 3));
+        const int color_idx =
+            (int)(((int)iters / (double)max_iter) * NUM_COLORS);
+        // determine percentage where iters is in the spectrum of the minimum of
+        // the current color and the next
+        const int needed_iters_idx =
+            (color_idx / (double)NUM_COLORS) * max_iter;
+        const int needed_iters_next =
+            ((color_idx + 1) / (double)NUM_COLORS) * max_iter;
+        const double interpolate =
+            (iters - needed_iters_idx) /
+            (double)(needed_iters_next - needed_iters_idx);
+        for (int j = 0; j < 3; j++) {
+          char col1 = color_idx < NUM_COLORS ? COLOR_SET1[color_idx][j] : 0;
+          char col2 = color_idx + 1 < NUM_COLORS ? COLOR_SET1[color_idx + 1][j] : 0;
+          img[i * 3 + j] =
+              (char)((int)col1 + interpolate * ((int)col2 - (int)col1));
+        }
+      } else {
+        // not diverging
+        for (int j = 0; j < 3; j++) {
+          img[i * 3 + j] = 0;
+        }
       }
     });
   });
